e4ac2baf1c58ffba07e9ef1ab87e5b6114128d6a
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2022-06-04 10:19:12 +0530

    Change in project code

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from pprint import pprint
 from gsheetpy import GoogleSheetConnector
 
-spreadsheeet = GoogleSheetConnector()
+spreadsheeet = GoogleSheetConnector(
+	scopes=["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"],
+	creds_path="D:\dev\gsheetspy\gsheetpy\creds.json"
+)
 
 sheet = spreadsheeet.open_sheet(
 	spreadsheet_id='spread_sheet_id',
